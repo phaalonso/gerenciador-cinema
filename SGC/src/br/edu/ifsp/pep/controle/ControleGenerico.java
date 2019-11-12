c69000aa8ec614cee231480e6031b67bc23f0988
pedro <phaalonso@gmail.com>
2019-11-12 14:29:26 +0000

    Renomeação da tabela super usuario

--- SGC/src/br/edu/ifsp/pep/controle/ControleGenerico.java
+++ SGC/src/br/edu/ifsp/pep/controle/ControleGenerico.java
@@ -8,10 +8,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaQuery;
-import org.eclipse.persistence.config.HintValues;
-import org.eclipse.persistence.config.QueryHints;
 
 /**
  *
--- SGC/src/br/edu/ifsp/pep/modelo/SuperUsuario.java
+++ SGC/src/br/edu/ifsp/pep/modelo/SuperUsuario.java
@@ -2,8 +2,10 @@ package br.edu.ifsp.pep.modelo;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "super_usuario")
 @DiscriminatorValue(value = "super")
 public class SuperUsuario extends UsuarioComum{
 
