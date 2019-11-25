21f5dcfdb5557f0494804d8d34620e2979ffa366
pedro <phaalonso@gmail.com>
2019-11-25 11:37:52 -0300

    Modificacoes

--- SGC/src/br/edu/ifsp/pep/visao/Menu.java
+++ SGC/src/br/edu/ifsp/pep/visao/Menu.java
@@ -31,80 +31,122 @@ public class Menu extends javax.swing.JFrame {
     private void initComponents() {
 
         PMain = new javax.swing.JPanel();
-        gPMenu = new com.hq.swingmaterialdesign.materialdesign.MGradientPanel();
-        jLabel1 = new javax.swing.JLabel();
-        mButton1 = new com.hq.swingmaterialdesign.materialdesign.MButton();
-        mButton6 = new com.hq.swingmaterialdesign.materialdesign.MButton();
-        mButton7 = new com.hq.swingmaterialdesign.materialdesign.MButton();
-        mButton8 = new com.hq.swingmaterialdesign.materialdesign.MButton();
-        mButton9 = new com.hq.swingmaterialdesign.materialdesign.MButton();
-        mButton10 = new com.hq.swingmaterialdesign.materialdesign.MButton();
+        pnMenu = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        btVenda = new com.hq.swingmaterialdesign.materialdesign.MButton();
+        btSair = new com.hq.swingmaterialdesign.materialdesign.MButton();
+        btHome = new com.hq.swingmaterialdesign.materialdesign.MButton();
+        btSessoes = new com.hq.swingmaterialdesign.materialdesign.MButton();
+        btFilmes = new com.hq.swingmaterialdesign.materialdesign.MButton();
+        btProdutos = new com.hq.swingmaterialdesign.materialdesign.MButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         PMain.setBackground(java.awt.Color.gray);
 
-        gPMenu.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        gPMenu.setBorderRadius(0);
-        gPMenu.setEndColor(new java.awt.Color(73, 136, 137));
-        gPMenu.setStartColor(new java.awt.Color(73, 136, 137));
-        gPMenu.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        pnMenu.setBackground(new java.awt.Color(73, 136, 137));
 
-        jLabel1.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
-        jLabel1.setText("SGC");
-        gPMenu.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(64, 54, -1, -1));
+        jLabel2.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jLabel2.setText("SGC");
 
-        mButton1.setBackground(new java.awt.Color(73, 136, 137));
-        mButton1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        mButton1.setText("VENDA");
-        gPMenu.add(mButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, 199, 192, 70));
+        btVenda.setBackground(new java.awt.Color(73, 136, 137));
+        btVenda.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btVenda.setText("VENDA");
 
-        mButton6.setBackground(new java.awt.Color(73, 136, 137));
-        mButton6.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        mButton6.setText("SAIR");
-        mButton6.addMouseListener(new java.awt.event.MouseAdapter() {
+        btSair.setBackground(new java.awt.Color(73, 136, 137));
+        btSair.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btSair.setText("SAIR");
+        btSair.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btSair.setInheritsPopupMenu(true);
+        btSair.setName(""); // NOI18N
+        btSair.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                mButton6MouseClicked(evt);
+                btSairMouseClicked(evt);
             }
         });
-        mButton6.addActionListener(new java.awt.event.ActionListener() {
+        btSair.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mButton6ActionPerformed(evt);
+                btSairActionPerformed(evt);
             }
         });
-        gPMenu.add(mButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, 463, 192, 70));
 
-        mButton7.setBackground(new java.awt.Color(37, 157, 218));
-        mButton7.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        mButton7.setText("Menu");
-        gPMenu.add(mButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, 133, 192, 70));
+        btHome.setBackground(new java.awt.Color(37, 157, 218));
+        btHome.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btHome.setText("Home");
 
-        mButton8.setBackground(new java.awt.Color(73, 136, 137));
-        mButton8.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        mButton8.setText("SESSÕES");
-        gPMenu.add(mButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, 265, 192, 70));
+        btSessoes.setBackground(new java.awt.Color(73, 136, 137));
+        btSessoes.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btSessoes.setText("SESSÕES");
 
-        mButton9.setBackground(new java.awt.Color(73, 136, 137));
-        mButton9.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        mButton9.setText("FILMES");
-        gPMenu.add(mButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, 331, 192, 70));
+        btFilmes.setBackground(new java.awt.Color(73, 136, 137));
+        btFilmes.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btFilmes.setText("FILMES");
 
-        mButton10.setBackground(new java.awt.Color(73, 136, 137));
-        mButton10.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        mButton10.setText("PRODUTOS");
-        gPMenu.add(mButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, 397, 192, 70));
+        btProdutos.setBackground(new java.awt.Color(73, 136, 137));
+        btProdutos.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btProdutos.setText("PRODUTOS");
+
+        javax.swing.GroupLayout pnMenuLayout = new javax.swing.GroupLayout(pnMenu);
+        pnMenu.setLayout(pnMenuLayout);
+        pnMenuLayout.setHorizontalGroup(
+            pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(pnMenuLayout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addGroup(pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(pnMenuLayout.createSequentialGroup()
+                            .addGap(66, 66, 66)
+                            .addComponent(jLabel2))
+                        .addComponent(btHome, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btSair, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btVenda, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btFilmes, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btProdutos, javax.swing.GroupLayout.DEFAULT_SIZE, 192, Short.MAX_VALUE)
+                        .addComponent(btSessoes, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+        pnMenuLayout.setVerticalGroup(
+            pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 640, Short.MAX_VALUE)
+            .addGroup(pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(pnMenuLayout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jLabel2)
+                    .addGap(46, 46, 46)
+                    .addGroup(pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(btHome, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(pnMenuLayout.createSequentialGroup()
+                            .addGap(330, 330, 330)
+                            .addComponent(btSair, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(pnMenuLayout.createSequentialGroup()
+                            .addGap(66, 66, 66)
+                            .addComponent(btVenda, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(pnMenuLayout.createSequentialGroup()
+                            .addGap(198, 198, 198)
+                            .addComponent(btFilmes, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(pnMenuLayout.createSequentialGroup()
+                            .addGap(264, 264, 264)
+                            .addComponent(btProdutos, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(pnMenuLayout.createSequentialGroup()
+                            .addGap(132, 132, 132)
+                            .addComponent(btSessoes, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
 
         javax.swing.GroupLayout PMainLayout = new javax.swing.GroupLayout(PMain);
         PMain.setLayout(PMainLayout);
         PMainLayout.setHorizontalGroup(
             PMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(PMainLayout.createSequentialGroup()
-                .addComponent(gPMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 582, Short.MAX_VALUE))
+                .addComponent(pnMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 577, Short.MAX_VALUE))
         );
         PMainLayout.setVerticalGroup(
             PMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(gPMenu, javax.swing.GroupLayout.DEFAULT_SIZE, 640, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PMainLayout.createSequentialGroup()
+                .addComponent(pnMenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -113,7 +155,7 @@ public class Menu extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(PMain, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 2, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -126,24 +168,24 @@ public class Menu extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void mButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mButton6ActionPerformed
+    private void btSairMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btSairMouseClicked
         // TODO add your handling code here:
-    }//GEN-LAST:event_mButton6ActionPerformed
+    }//GEN-LAST:event_btSairMouseClicked
 
-    private void mButton6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_mButton6MouseClicked
-        dispose();
-    }//GEN-LAST:event_mButton6MouseClicked
+    private void btSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btSairActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btSairActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel PMain;
-    private com.hq.swingmaterialdesign.materialdesign.MGradientPanel gPMenu;
-    private javax.swing.JLabel jLabel1;
-    private com.hq.swingmaterialdesign.materialdesign.MButton mButton1;
-    private com.hq.swingmaterialdesign.materialdesign.MButton mButton10;
-    private com.hq.swingmaterialdesign.materialdesign.MButton mButton6;
-    private com.hq.swingmaterialdesign.materialdesign.MButton mButton7;
-    private com.hq.swingmaterialdesign.materialdesign.MButton mButton8;
-    private com.hq.swingmaterialdesign.materialdesign.MButton mButton9;
+    private com.hq.swingmaterialdesign.materialdesign.MButton btFilmes;
+    private com.hq.swingmaterialdesign.materialdesign.MButton btHome;
+    private com.hq.swingmaterialdesign.materialdesign.MButton btProdutos;
+    private com.hq.swingmaterialdesign.materialdesign.MButton btSair;
+    private com.hq.swingmaterialdesign.materialdesign.MButton btSessoes;
+    private com.hq.swingmaterialdesign.materialdesign.MButton btVenda;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel pnMenu;
     // End of variables declaration//GEN-END:variables
 }
