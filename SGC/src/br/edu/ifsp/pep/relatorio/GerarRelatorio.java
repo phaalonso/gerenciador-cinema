6f59b85624b50b1b015fc0bfbd8d53ddac83e3f9
SupNL <gustavoonishinl@gmail.com>
2019-12-01 15:11:18 -0300

    Conclusão dos relatórios

--- SGC/src/br/edu/ifsp/pep/relatorio/GerarRelatorio.java
+++ SGC/src/br/edu/ifsp/pep/relatorio/GerarRelatorio.java
@@ -37,6 +37,32 @@ public class GerarRelatorio {
         }catch(JRException ex){
             System.err.println(ex.getMessage());
         }
-        
+    }
+    
+    public void gerarSessao(EntityManager em, String relatorio,
+            Date dataInicial, Date dataFinal){
+        try{
+            // Parametros
+            HashMap<String, Object> parametros = new HashMap<>();
+            parametros.put(
+                    JRJpaQueryExecuterFactory.PARAMETER_JPA_ENTITY_MANAGER,
+                    em
+            );
+            parametros.put("dataInicial", dataInicial);
+            parametros.put("dataFinal", dataFinal);
+            
+            // Carregar o relatório
+            InputStream jasper = getClass().getResourceAsStream(
+                    "/br/edu/ifsp/pep/relatorio/" + relatorio);
+            
+            // Preencher com os dados
+            JasperPrint jp = JasperFillManager.fillReport(jasper, parametros);
+            
+            // Exibir
+            JasperViewer jv = new JasperViewer(jp, true);
+            jv.setVisible(true);
+        }catch(JRException ex){
+            System.err.println(ex.getMessage());
+        }
     }
 }
--- SGC/src/testeRelatorio.java
+++ SGC/src/testeRelatorio.java
@@ -7,11 +7,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 public class testeRelatorio {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, InterruptedException {
         EntityManager em = Persistence.createEntityManagerFactory("SGCPU").createEntityManager();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date dataInicial = sdf.parse("2019-01-01");
         Date dataFinal = sdf.parse("2019-01-02");
         new GerarRelatorio().gerarVendaProduto(em, "RelatorioProduto.jasper", dataInicial, dataFinal);
+        Thread.currentThread().sleep(200);
+        dataInicial = sdf.parse("2019-11-01");
+        dataFinal = sdf.parse("2019-11-01");
+        new GerarRelatorio().gerarSessao(em, "RelatorioSessao.jasper", dataInicial, dataFinal);
     }
 }
